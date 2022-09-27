12e1aac6571b0c9eeb2df09c71d7da1d631d56b2
scottchoo <scottchoo@a41.io>
2022-09-27 13:08:21 +0900

    chore: change tailwind.config

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "948de5b06c121a44"; }
+/******/ 		__webpack_require__.h = function() { return "c912e8ae4a72172e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
